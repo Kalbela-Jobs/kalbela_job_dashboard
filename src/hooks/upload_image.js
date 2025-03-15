00da622bbc70ff1dbe7995d41a2d52cfabfc5494
codewithmahadihasan <73072248+codewithmahadihasan@users.noreply.github.com>
2025-03-15 06:27:20 +0600

    update

--- src/hooks/upload_image.js
+++ src/hooks/upload_image.js
@@ -8,8 +8,9 @@ const uploadImage = async (file) => {
             const formData = new FormData();
             formData.append("image", file);
 
-            // const url = `http://localhost:5005/api/v1/image/upload-image`;
-            const url = `https://server.kalbelajobs.com/api/v1/image/upload-image`;
+
+            // const url = `http://localhost:5005/api/v2/image/upload-image`;
+            const url = `https://server.kalbelajobs.com/api/v2/image/upload-image`;
             const response = await fetch(url, {
                   method: "PUT",
                   body: formData,
